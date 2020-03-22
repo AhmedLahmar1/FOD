dea72166fb184b167d81ee9359612ce491f45128
Ahmed <lahmara7@gmail.com>
2020-03-22 16:34:20 +0100

    backSignup_LogIn

--- pfaProject/ASP.NET/PfaBackFinal/Controllers/ApplicationUserController.cs
+++ pfaProject/ASP.NET/PfaBackFinal/Controllers/ApplicationUserController.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using HdBackApp.Models;
 using HindiBackApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HindiBackApp.Controllers
 {
@@ -15,11 +21,14 @@ namespace HindiBackApp.Controllers
     {
         private UserManager<HindiBackApp.Models.ApplicationUser> _userManager;
         private SignInManager<HindiBackApp.Models.ApplicationUser> _singInManager;
+        private readonly ApplicationSettings _appSettings;
 
-        public ApplicationUserController(UserManager<Models.ApplicationUser> userManager, SignInManager<HindiBackApp.Models.ApplicationUser> signInManager)
+
+        public ApplicationUserController(UserManager<Models.ApplicationUser> userManager, SignInManager<HindiBackApp.Models.ApplicationUser> signInManager, IOptions<ApplicationSettings> appSettings)
         {
             this._userManager = userManager;
             this._singInManager = signInManager;
+            this. _appSettings = appSettings.Value;
         }
 
         [HttpPost]
@@ -45,5 +54,32 @@ namespace HindiBackApp.Controllers
                 throw ex;
             }
         }
+
+        [HttpPost]
+        [Route("Login")]
+        //POST : /api/ApplicationUser/Login
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim("UserID",user.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(10),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                var token = tokenHandler.WriteToken(securityToken);
+                return Ok(new { token });
+            }
+            else
+                return BadRequest(new { message = "Username or password is incorrect." });
+        }
+
     }
 }
--- pfaProject/ASP.NET/PfaBackFinal/Startup.cs
+++ pfaProject/ASP.NET/PfaBackFinal/Startup.cs
@@ -12,7 +12,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using HdBackApp.Models;
 
 namespace HindiBackApp
 {
@@ -28,6 +31,7 @@ namespace HindiBackApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApplicationSettings>(Configuration.GetSection("ApplicationSettings"));
             services.AddControllers();
             services.AddDbContext<AuthentificationContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
@@ -49,7 +53,27 @@ namespace HindiBackApp
             );
 
             services.AddCors();
+            //Jwt Authentication
 
+            var key = Encoding.UTF8.GetBytes(Configuration["ApplicationSettings:JWT_Secret"].ToString());
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = false;
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
         }
 
@@ -71,7 +95,7 @@ namespace HindiBackApp
             }
 
             app.UseCors(builder =>
-            builder.WithOrigins("http://localhost:4200")
+            builder.WithOrigins(Configuration["ApplicationSettings:Client_URL"].ToString())
             .AllowAnyHeader()
             .AllowAnyMethod()
 
